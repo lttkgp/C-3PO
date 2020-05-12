ceba23468d375210b08e91a05c7d708f50bf80ed
Parth Paradkar <parthparadkar3@gmail.com>
2020-05-12 23:08:18 +0530

    Add makeshift endpoint

--- c3po/api/controller/feed_controller.py
+++ c3po/api/controller/feed_controller.py
@@ -1,14 +1,14 @@
 from flask import abort, request
 from flask_restx import Resource, Namespace, reqparse
-from flask_restx.inputs import date_from_iso8601
-from ..service.feed_service import get_all
+from flask_restx.inputs import datetime_from_iso8601
+from ..service.feed_service import get_from_to
 api = Namespace('feed', description='LTTKGP Feed')
 
 parser = reqparse.RequestParser()
 parser.add_argument('limit', type=int)
 parser.add_argument('genre', type=str)
-parser.add_argument('from', type=date_from_iso8601)
-parser.add_argument('to', type=date_from_iso8601)
+parser.add_argument('from', type=datetime_from_iso8601)
+parser.add_argument('to', type=datetime_from_iso8601)
 
 '''
 limit - to limit the number of responses (although we’ll use a default value if this is not specified)
@@ -25,18 +25,13 @@ class FeedPopular(Resource):
     """ User Login Resource """
     @api.doc('Popular songs (most liked)')
     @api.expect(parser)
-    def get(self):
+    def get(self): 
         args = parser.parse_args()
-        response_object = get_all()
+        response_object = get_from_to(args['from'], args['to'])
         return response_object, 200
 
 @api.route('/latest')
 class FeedLatest(Resource):
     @api.doc('Latest feed')
     def get(self):
-        response_object = {
-            'status' : 'Success! latest, bitch!!',
-            'message' : '__init__()'
-        }
-        
         return response_object, 200
--- c3po/api/service/feed_service.py
+++ c3po/api/service/feed_service.py
@@ -1,12 +1,17 @@
 from c3po.db.dao.user import UserPosts
 from c3po.db.dao.artist import ArtistSong
 from c3po.db.common.base import session_scope
+from datetime import datetime, timedelta
 
-def get_all():
+DEFAULT_TIME_PERIOD = timedelta(days=7)
+
+def get_from_to(from_=datetime.now(), to_=datetime.now() - DEFAULT_TIME_PERIOD):
     with session_scope() as session:
-        posts = session.query(UserPosts).all()
+        posts = session.query(UserPosts).filter(UserPosts.share_date >= from_)
+        posts = posts.filter(UserPosts.share_date <= to_).all()
         return [
             {
-                'caption': obj.caption
+                'caption': obj.caption,
+                'url': obj.link.url
             } for obj in posts
         ]
