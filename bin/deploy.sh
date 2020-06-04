7cb8c3702fd86f9da67c60e1767b3120e2860778
Mukul Mehta <mukul.csiitkgp@gmail.com>
2020-06-04 16:10:16 +0530

    Update deploy script to install dependencies

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,5 +2,15 @@ cd /home/ubuntu/C-3PO
 
 git pull origin master
 
+export PATH="/home/ubuntu/.pyenv/bin:$PATH"
+eval "$( command pyenv init - )"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv local C-3PO
+
+pip install -r requirements/common.txt
+pip install -r requirements/dev.txt
+pip install -e .
+
 sudo systemctl restart c3po.service
 
