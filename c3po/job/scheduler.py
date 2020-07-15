31cc617dba8f167b305a7effad344edbc66ddc7e
Mukul Mehta <mukul.csiitkgp@gmail.com>
2020-07-16 00:47:53 +0530

    Stitch both scheduler processes into single function

--- c3po/job/scheduler.py
+++ c3po/job/scheduler.py
@@ -1,3 +1,4 @@
+import datetime
 from logging import getLogger
 
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -13,18 +14,35 @@ PROCESS_POSTS_FREQUENCY = int(
 )
 FB_FETCH_FREQUENCY = int(read_config(section="scheduler")["FB_FETCH_FREQUENCY"])
 
+def fetch_and_process():
+    try:
+        LOG.info(f"Starting scheduled processes at {datetime.datetime.now()}")
+        update_posts_db()
+        LOG.info(f"Done with fetching posts from FB at {datetime.datetime.now()}")
+        first_time_init()
+        LOG.info(f"Done with processing posts at {datetime.datetime.now()}")
+    
+    except Exception:
+        LOG.error("There was an error updating posts", exc_info=True)
 
 def setup_scheduler(fetch_fb_posts=True, process_posts=True):
-    if fetch_fb_posts:
-        sched.add_job(update_posts_db, "interval", minutes=FB_FETCH_FREQUENCY)
+    if fetch_fb_posts and process_posts:
+        sched.add_job(fetch_and_process, "interval", minutes=FB_FETCH_FREQUENCY)
         LOG.info(
-            f"Add scheduler job to Fetch FB posts every {FB_FETCH_FREQUENCY} minutes"
+            f"Add scheduler job to Fetch and Process FB posts every {FB_FETCH_FREQUENCY} minutes"
         )
-
-    if process_posts:
-        sched.add_job(first_time_init, "interval", minutes=PROCESS_POSTS_FREQUENCY)
-        LOG.info(
-            f"Add scheduler job to process Mongo posts every {PROCESS_POSTS_FREQUENCY} minutes"
-        )
-
+    
+    if fetch_fb_posts ^ process_posts:
+        if fetch_fb_posts:
+            sched.add_job(update_posts_db, "interval", minutes=FB_FETCH_FREQUENCY)
+            LOG.info(
+                f"Add scheduler job to Fetch FB posts every {FB_FETCH_FREQUENCY} minutes"
+            )
+        
+        if process_posts:
+            sched.add_job(first_time_init, "interval", minutes=PROCESS_POSTS_FREQUENCY)
+            LOG.info(
+                f"Add scheduler job to process Mongo posts every {PROCESS_POSTS_FREQUENCY} minutes"
+            )
+    
     return sched
