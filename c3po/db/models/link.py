14f04334a19e53b266d171aa88ca0a343dbd3b9b
Yeshu <2000yeshu@gmail.com>
2020-12-23 19:22:43 +0530

    support yt music

--- c3po/db/models/link.py
+++ c3po/db/models/link.py
@@ -1,5 +1,11 @@
-from sqlalchemy import BigInteger, Column, Float, ForeignKey, Integer, String
-from sqlalchemy.orm import backref, relationship
+from sqlalchemy import BigInteger
+from sqlalchemy import Column
+from sqlalchemy import Float
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import backref
+from sqlalchemy.orm import relationship
 
 from c3po.db.base import Base
 
@@ -11,7 +17,7 @@ class Link(Base):
     id = Column(Integer, primary_key=True)
     url = Column("url", String(160))
     platform = Column("platform", Integer)
-
+    original_url = Column("original_url", String)
     song_id = Column(Integer, ForeignKey("song.id"))
 
     post_count = Column("post_count", Integer)
@@ -19,11 +25,12 @@ class Link(Base):
     views = Column("views", BigInteger)
     custom_popularity = Column("custom_popularity", Float)
 
-    def __init__(self, url, platform, custom_popularity, views):
+    def __init__(self, url, platform, custom_popularity, views, original_url):
         self.url = url
         self.platform = platform
         self.custom_popularity = custom_popularity
         self.views = views
+        self.original_url = original_url
 
     def __str__(self):
         return self.url
